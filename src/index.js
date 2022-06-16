366773e5ff380ccca8779e5125f332d1a21e59f3
Rayan <53921750+binelias@users.noreply.github.com>
2022-06-16 18:51:08 +0800

    transform to react redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css'
+import thunkMiddleware from 'redux-thunk';
 // import Card from './Card';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
 // import { robots } from './robots';
+
+const logger = createLogger();
+
+const rootReducers = combineReducers({ searchRobots, requestRobots});
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <App/>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
